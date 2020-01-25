ada87dc66167138f1217565330436848c8418218
ganesh padole <ganeshpadole55@gmail.com>
2020-01-25 13:56:55 +0530

    testing ....

--- MemoryIntroduction/ViewController.swift
+++ MemoryIntroduction/ViewController.swift
@@ -13,27 +13,50 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        var first: MyClass? = MyClass()
-        var second = first
-        var third = first
+//        var first: MyClass? = MyClass()
+//        var second = first
+//        var third = first
+//
+//        third = nil
+//        second = nil
+//        first = nil
         
-        third = nil
-        second = nil
-        first = nil
+        
+        var father: Parent?
+        
+        father = Parent()
+        father = nil
+        
     }
 
 
 }
 
-class MyClass {
-    
-    var myInt = 10
-    
-    init() {
-        print("MyClass initiliazes....")
+//class MyClass {
+//
+//    var myInt = 10
+//
+//    init() {
+//        print("MyClass initiliazes....")
+//    }
+//
+//    deinit {
+//        print("MyClass will be deallocated...")
+//    }
+//}
+
+
+class Child {
+    deinit {
+        print("Child get deinit...")
     }
+}
+
+class Parent {
+    
+    var child = Child()
     
     deinit {
-        print("MyClass will be deallocated...")
+        print("Parent get deinited....")
     }
 }
